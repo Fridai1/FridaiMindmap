77a862e48bdb52a03a3e0fff7a75c685da5b85a1
Nikolai Christiansen <nikolaiviby3@gmail.com>
2026-03-22 15:49:02 +0100

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import './../app.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<nav class="sidebar">
+	<a href="/">Home</a>
+	<a href="/brain">Brain</a>
+</nav>
+<main>
+	{@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="main-container">This will be the main body</div>
+
+<style>
+	.main-container {
+		background-color: blanchedalmond;
+		border-radius: 0.5rem;
+		width: 60%;
+		display: flex;
+	}
+</style>
